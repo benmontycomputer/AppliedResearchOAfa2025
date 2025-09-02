655c569979747e74ba98d048403ab36ab8f4f1bc
plenjamin <53486418+benmontycomputer@users.noreply.github.com>
2025-09-02 19:38:54 -0400

    Start huggingface

---
+++ submissions/Benjamin Montgomery/README.md
@@ -0,0 +1,3 @@
+Python version used: 3.11.11
+
+pip install transformers datasets evaluate accelerate torch SentencePiece
---
+++ submissions/Benjamin Montgomery/generate_calendar.py
@@ -0,0 +1,12 @@
+# pip install accelerate
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl", device_map="auto")
+
+input_text = "translate English to German: How old are you?"
+# TODO: update this depending on what device we are running on
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("mps")#.to("cuda")
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
